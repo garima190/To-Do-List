cd1e40b89d38b3b82018a6d999a88c6c2b3b7f98
Garima Singh <63351001+garima190@users.noreply.github.com>
2022-03-19 09:19:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useState ,useEffect} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+const App= () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+useEffect(() => {
+  const getTasks = async () =>{
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
+  }
+  getTasks()
+}, [])
+  
+const fetchTasks = async ()=>{
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+  return data
+}
+//Add Task
+const addTask=async(task)=>{
+   const res = await fetch('http://localhost:5000/tasks',{
+     method: 'POST',
+     headers:{
+       'Content-type':'application/json',
+     },
+     body: JSON.stringify(task),
+   })
+   const data=await res.json()
+   setTasks([...tasks, data])
+  
+}
+//delete task
+const deleteTask=async(id)=>{
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE',
+  })
+  setTasks(tasks.filter((task)=>task.id !== id))
+}
+
+  return (
+    <div className='container'>
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length>0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask} 
+       />
+      ):(
+        'No Tasks To show'
+      )}
+    </div>
+  )
+}
+
+export default App;
